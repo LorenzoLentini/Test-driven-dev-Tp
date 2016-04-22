133adb26c9308e3f2a628166d321d08986179846
Pierre Dupond <pierre.dupond@univ-lille1.fr>
2016-04-22 09:55:09 +0200

    everyone

--- src/main/java/iut/tdd/RomanNumerals.java
+++ src/main/java/iut/tdd/RomanNumerals.java
@@ -2,8 +2,15 @@ package iut.tdd;
 
 public class RomanNumerals {
 	public String convertToRoman(String arabe) {
+		
+		if (arabe.equals("1")){
 		return "I";
-	}
+		}
+		else if (arabe.equals("2")){
+			return "II";
+		}
+		return " ";
+		}
 
 	public Object convertFromRoman(String roman) {
 		return "1";
--- src/test/java/iut/tdd/RomanNumeralsTest.java
+++ src/test/java/iut/tdd/RomanNumeralsTest.java
@@ -6,15 +6,27 @@ import org.junit.Test;
 public class RomanNumeralsTest {
 
 	@Test
-	public void should_return_I_when_1 () {
-		//Given
+	public void should_return_I_when_1() {
+		// Given
 		String input = "1";
 		String expected = "I";
 		RomanNumerals romanNumeral = new RomanNumerals();
-		//When
+		// When
 		String actual = romanNumeral.convertToRoman(input);
-		//Then
+		// Then
 		Assert.assertEquals(expected, actual);
 	}
-	
+
+	@Test
+	public void should_return_II_when_2() {
+		// Given
+		String input = "2";
+		String expected = "II";
+		RomanNumerals romanNumeral = new RomanNumerals();
+		// When
+		String actual = romanNumeral.convertToRoman(input);
+		// Then
+		Assert.assertEquals(expected, actual);
+	}
+
 }
